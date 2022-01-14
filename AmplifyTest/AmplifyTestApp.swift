f7507c1806650a2233f29c939270657b56f5585b
InhwanKim <ianhenry.gim@gmail.com>
2022-01-14 14:42:15 +0900

    initial commit

---
+++ AmplifyTest/AmplifyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmplifyTestApp.swift
+//  AmplifyTest
+//
+//  Created by inhwankim on 2022/01/14.
+//
+
+import SwiftUI
+
+@main
+struct AmplifyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AmplifyTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AmplifyTest
+//
+//  Created by inhwankim on 2022/01/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
